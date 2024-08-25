87e6fd0ef50b4e8e725dd44246ea6586056d2478
ningalu <thomasholster2012@gmail.com>
2024-08-25 20:12:48 +1200

    port polling input

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <memory>
 #include <string>
+#include <variant>
 
-#include "lib.hpp"
+#include "tbc/Battle.hpp"
+#include "tbc/Command.hpp"
+#include "tbc/PlayerComms.hpp"
 
-auto main() -> int
-{
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
+#include "tbc/lib.hpp"
+
+using MyCommands = ngl::tbc::Command<int, double>;
+auto main() -> int {
+  std::cout << "ningalu tbc\n";
+
+  auto p1 = std::make_unique<ngl::tbc::PlayerComms<MyCommands>>("Player 1", []() { return std::vector<MyCommands>({MyCommands{1}}); }, [](std::size_t) { return MyCommands(1.0); });
+  auto p2 = std::make_unique<ngl::tbc::PlayerComms<MyCommands>>("Player 2", []() { return std::vector<MyCommands>({MyCommands{2.0}}); }, [](std::size_t) { return MyCommands(1); });
+  std::vector<std::unique_ptr<ngl::tbc::PlayerComms<MyCommands>>> players;
+  players.emplace_back(std::move(p1));
+  players.emplace_back(std::move(p2));
+  auto b = ngl::tbc::Battle<int, MyCommands>{std::move(players)};
+
+  for (const auto &r : b.RequestStaticCommands()) {
+    std::visit([](const auto &p) { std::cout << p << "\n"; }, r.payload);
+  }
   return 0;
 }
