1d06d6cd840c12e894dd540260beecf9b0634c7c
ningalu <thomasholster2012@gmail.com>
2024-09-15 21:25:12 +1200

    first basically working impl

--- include/tbc/Event.hpp
+++ include/tbc/Event.hpp
@@ -9,22 +9,24 @@
 #include "tbc/Action.hpp"
 
 namespace ngl::tbc {
+namespace DefaultEvents {
+
+struct BattleStart {};
+struct BattleEnd {
+  std::vector<std::size_t> winners;
+};
+
+struct TurnsStart {};
+struct TurnsEnd {};
+
+struct ActionStart {};
+struct ActionEnd {};
+} // namespace DefaultEvents
 
 // TODO: decouple from TBattle; find a way to define just the event payload while also getting the Callback tuple into EventHandler
 template <typename TBattle, typename... Ts>
 struct Event {
 public:
-  struct BattleStart {};
-  struct BattleEnd {
-    std::vector<std::size_t> winners;
-  };
-
-  struct TurnsStart {};
-  struct TurnsEnd {};
-
-  struct ActionStart {};
-  struct ActionEnd {};
-
   template <typename TSpecificEvent>
   using Callback = std::function<Action<TBattle>(TSpecificEvent)>;
 
@@ -35,7 +37,15 @@ private:
     using Handlers = std::tuple<Callback<Ts>...>;
   };
 
-  using Events = Detail<TBattle, BattleStart, BattleEnd, TurnsStart, TurnsEnd, ActionStart, ActionEnd, Ts...>;
+  using Events = Detail<
+    TBattle,
+    DefaultEvents::BattleStart,
+    DefaultEvents::BattleEnd,
+    DefaultEvents::TurnsStart,
+    DefaultEvents::TurnsEnd,
+    DefaultEvents::ActionStart,
+    DefaultEvents::ActionEnd,
+    Ts...>;
 
 public:
   using Battle   = TBattle;
