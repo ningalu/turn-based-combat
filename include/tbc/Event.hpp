cfa988efc1bf828f189b82c84ebd91af541554ba
ningalu <thomasholster2012@gmail.com>
2024-09-19 16:06:00 +1200

    Decouple Event and Battle

--- include/tbc/Event.hpp
+++ include/tbc/Event.hpp
@@ -1,12 +1,7 @@
 #ifndef TBC_EVENT_HPP
 #define TBC_EVENT_HPP
 
-#include <functional>
-#include <tuple>
 #include <variant>
-#include <vector>
-
-#include "tbc/Action.hpp"
 
 namespace ngl::tbc {
 namespace DefaultEvents {
@@ -23,35 +18,27 @@ struct ActionStart {};
 struct ActionEnd {};
 } // namespace DefaultEvents
 
+namespace detail {
+template <typename... TPayloads>
+struct EventDetail {
+  using Payload = std::variant<TPayloads...>;
+};
+} // namespace detail
+
 // TODO: decouple from TBattle; find a way to define just the event payload while also getting the Callback tuple into EventHandler
-template <typename TBattle, typename... Ts>
+template <typename... TUserPayloads>
 struct Event {
-public:
-  template <typename TSpecificEvent>
-  using Callback = std::function<Action<TBattle>(TSpecificEvent)>;
 
-private:
-  template <typename TBattle, typename... Ts>
-  struct Detail {
-    using Payload  = std::variant<Ts...>;
-    using Handlers = std::tuple<Callback<Ts>...>;
-  };
-
-  using Events = Detail<
-    TBattle,
+  using Detail = detail::EventDetail<
     DefaultEvents::BattleStart,
     DefaultEvents::BattleEnd,
     DefaultEvents::TurnsStart,
     DefaultEvents::TurnsEnd,
     DefaultEvents::ActionStart,
     DefaultEvents::ActionEnd,
-    Ts...>;
+    TUserPayloads...>;
 
-public:
-  using Battle   = TBattle;
-  using Payload  = Events::Payload;
-  using Handlers = Events::Handlers;
-
+  using Payload = typename Detail::Payload;
   Payload payload;
 };
 } // namespace ngl::tbc
--- include/tbc/EventHandler.hpp
+++ include/tbc/EventHandler.hpp
@@ -8,36 +8,58 @@
 #include "tbc/Event.hpp"
 
 namespace ngl::tbc {
-template <typename TEvents>
-class EventHandler {
-  using Battle = TEvents::Battle;
+
+namespace detail {
+template <typename TBattle, typename TEvent>
+using EventHandlerCallback = std::function<Action<TBattle>(TEvent)>;
+}
+
+template <typename TBattle, typename TEvent>
+class EventHandler;
+
+template <typename TBattle, typename... TUserPayloads>
+class EventHandler<TBattle, Event<TUserPayloads...>> {
+public:
+  template <typename TSpecificEvent>
+  using Callback = detail::EventHandlerCallback<TBattle, TSpecificEvent>;
+
+protected:
+  template <typename TEventDetail>
+  struct HandlerDetail;
+
+  template <typename... TPayloads>
+  struct HandlerDetail<detail::EventDetail<TPayloads...>> {
+    using Callbacks = std::tuple<Callback<TPayloads>...>;
+  };
+
+  using Callbacks = HandlerDetail<typename Event<TUserPayloads...>::Detail>::Callbacks;
 
 public:
   template <typename TSpecificEvent>
   [[nodiscard]] bool HasHandler() const {
-    return static_cast<bool>(std::get<TEvents::Callback<TSpecificEvent>>(callbacks_));
+    return static_cast<bool>(std::get<Callback<TSpecificEvent>>(callbacks_));
   }
 
-  // Why doesn't TEvents::Callback<TSpecificEvent> work as the function argument?
   template <typename TSpecificEvent>
-  void RegisterHandler(std::function<Action<Battle>(TSpecificEvent)> handler) {
-    std::get<TEvents::Callback<TSpecificEvent>>(callbacks_) = handler;
+  void RegisterHandler(const Callback<TSpecificEvent> &handler) {
+    std::get<Callback<TSpecificEvent>>(callbacks_) = handler;
   }
 
   template <typename TSpecificEvent>
   void ResetHandler() {
-    std::get<TEvents::Callback<TSpecificEvent>>(callbacks_) = nullptr;
+    std::get<Callback<TSpecificEvent>>(callbacks_) = nullptr;
   }
 
   template <typename TSpecificEvent>
-  [[nodiscard]] Action<Battle> PostEvent(const TSpecificEvent &e) const {
+  [[nodiscard]] Action<TBattle> PostEvent(const TSpecificEvent &e) const {
     assert(HasHandler<TSpecificEvent>());
-    return std::get<TEvents::Callback<TSpecificEvent>>(callbacks_)(e);
+    return std::get<Callback<TSpecificEvent>>(callbacks_)(e);
   }
 
 protected:
-  TEvents::Handlers callbacks_;
+  Callbacks callbacks_;
 };
+
 } // namespace ngl::tbc
 
 #endif
