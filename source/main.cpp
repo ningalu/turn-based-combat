12297c533f9b35ead4fa0dab331aba21376dbcda
ningalu <thomasholster2012@gmail.com>
2024-10-02 21:34:21 +1300

    Add simple tests in sample project

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,10 @@
 #include <set>
 #include <stdexcept>
 #include <string>
+#include <utility>
 #include <variant>
+
+#include "util.h"
 
 #include "tbc/Battle.hpp"
 #include "tbc/BattleScheduler.hpp"
@@ -38,15 +41,8 @@ using MyEvents = ngl::tbc::Event<int, double>;
 
 using MyBattleTypes = ngl::tbc::BattleTypes<int, MyBattleState, MyCommands, MyEvents>;
 
-// using MyBattle       = ngl::tbc::Battle<int, MyBattleState>;
-// using MyEventHandler = ngl::tbc::EventHandler<MyBattle, MyEvents>;
-
-// using MyEffect        = ngl::tbc::Effect<MyBattle>;
-// using MyDefEffect     = ngl::tbc::DeferredEffect<MyBattle>;
-// using MyUserEffect    = ngl::tbc::UserEffect<MyBattle>;
-// using MyDefUserEffect = ngl::tbc::DeferredUserEffect<MyBattle>;
-
 using MyBattle       = MyBattleTypes::Battle;
+using MyScheduler    = MyBattleTypes::BattleScheduler;
 using MyEventHandler = MyBattleTypes::EventHandler;
 
 using MyEffect        = MyBattleTypes::Effect;
@@ -56,7 +52,13 @@ using MyDefUserEffect = MyBattleTypes::DeferredUserEffect;
 
 using MyResult = MyBattleTypes::EffectResult;
 
-const MyEffect debug_effect{
+using MyComms = MyBattleTypes::PlayerComms;
+
+MyEffect DebugEffect(int n) {
+  return {[=](auto &&...) {std::cout << "effect resolution: " << n << "\n"; return MyResult{}; }};
+}
+
+const MyEffect end_battle_effect{
   [](MyBattle &b, const std::vector<ngl::tbc::Target> &) { std::cout << "debug effect\n"; b.EndBattle({0}); return MyResult{}; }
 };
 
@@ -85,6 +87,21 @@ MyUserEffect GetEffect(int state) {
     return MyResult{}; }};
 }
 
+MyUserEffect GetEffectWithIntEvent(int state) {
+  return MyUserEffect{[=](ngl::tbc::Slot::Index u, MyBattle &b, const std::vector<ngl::tbc::Target> &) {
+    switch (u.side) {
+      case 0: b.p1_state = state; break;
+      case 1: b.p2_state = state; break;
+      default: throw std::logic_error{"invalid side"};
+    }
+    std::cout << "setting " << u.side << " to " << state << "\n";
+    MyResult out;
+    MyEvents e;
+    e.payload = 2;
+    std::get<3>(out).events.push_back(e);
+    return out; }};
+}
+
 using MyAction = MyBattleTypes::Action;
 MyAction GetAction(ngl::tbc::Slot::Index user, const std::vector<ngl::tbc::Target> &targets, int state) {
   if (state > 2) {
@@ -95,13 +112,97 @@ MyAction GetAction(ngl::tbc::Slot::Index user, const std::vector<ngl::tbc::Targe
   return MyAction{e};
 }
 
-MyEventHandler eh;
+MyAction GetActionWithIntEffect(ngl::tbc::Slot::Index user, const std::vector<ngl::tbc::Target> &targets, int state) {
+  if (state > 2) {
+    throw std::logic_error{"invalid state"};
+  }
+
+  MyDefUserEffect e{user, {GetEffectWithIntEvent(state)}, targets};
+  return MyAction{e};
+}
+
+std::function<std::vector<MyCommandPayload>()> GetComms(int n) {
+  assert(n < 3);
+  return [=]() {
+    switch (n) {
+    case 0:
+      return std::vector<MyCommandPayload>({MyCommandPayload{RockCommand{}}});
+    case 1:
+      return std::vector<MyCommandPayload>({MyCommandPayload{PaperCommand{}}});
+    case 2:
+      return std::vector<MyCommandPayload>({MyCommandPayload{ScissorsCommand{}}});
+    default:
+      ngl::unreachable();
+    }
+  };
+};
+
+auto default_validator  = [](std::size_t, const std::vector<MyCommandPayload> &payload, const std::vector<MyCommands> &) { return payload; };
+auto default_translator = [](const std::vector<MyCommands> &commands) {
+  auto out = std::vector<MyAction>{};
+  for (const auto &c : commands) {
+    int move = std::visit([](auto &&p) {
+      using T = std::decay_t<decltype(p)>;
+      if constexpr (std::is_same_v<T, RockCommand>) {
+        return 0;
+      } else if constexpr (std::is_same_v<T, PaperCommand>) {
+        return 1;
+      } else if constexpr (std::is_same_v<T, ScissorsCommand>) {
+        return 2;
+      } else {
+        throw std::logic_error{"invalid move"};
+      }
+    },
+                          c.payload);
+
+    out.push_back(GetAction({c.player, 0}, {}, move));
+  }
+  return out;
+};
+
+auto int_event_translator = [](const std::vector<MyCommands> &commands) {
+  auto out = std::vector<MyAction>{};
+  for (const auto &c : commands) {
+    int move = std::visit([](auto &&p) {
+      using T = std::decay_t<decltype(p)>;
+      if constexpr (std::is_same_v<T, RockCommand>) {
+        return 0;
+      } else if constexpr (std::is_same_v<T, PaperCommand>) {
+        return 1;
+      } else if constexpr (std::is_same_v<T, ScissorsCommand>) {
+        return 2;
+      } else {
+        throw std::logic_error{"invalid move"};
+      }
+    },
+                          c.payload);
+
+    out.push_back(GetActionWithIntEffect({c.player, 0}, {}, move));
+  }
+  return out;
+};
+
+MyAction default_turnend(ngl::tbc::DefaultEvents::TurnsEnd) {
+  return MyAction{MyDefEffect{{resolve_rps_effect}, {}}};
+}
+
+MyAction default_intevent(int n) {
+  std::cout << "int event received: " << n << "\n";
+  return MyAction{MyDefEffect{{DebugEffect(n)}, {}}};
+}
+
+auto default_layout = ngl::tbc::Layout{{{{0}}, {{1}}}};
+
+void test_battle_end();
+void test_user_event();
 
 auto main() -> int {
-  eh.RegisterHandler<int>([&](int a) {std::cout << "int: " << a << "\n"; return GetAction({0, 0}, {}, 0); });
-  std::cout << "ningalu tbc\n";
+  std::cout << "Test battle end\n";
+  test_battle_end();
+  std::cout << "\n\nTest user event\n";
+  test_user_event();
 
-  auto p1 = std::make_unique<ngl::tbc::PlayerComms<MyCommandPayload>>("Player 1", [&]() {
+  auto input_comms = [&]() {
     std::set<std::string> valid{"R", "P", "S"};
     std::cout << "Play R | P | S\n";
     std::string in;
@@ -115,65 +216,67 @@ auto main() -> int {
     } else {
       return std::vector<MyCommandPayload>({MyCommandPayload{ScissorsCommand{}}});
     }
-  });
-  auto p2 = std::make_unique<ngl::tbc::PlayerComms<MyCommandPayload>>("Player 2", []() { return std::vector<MyCommandPayload>({MyCommandPayload{ScissorsCommand{}}}); });
-  std::vector<std::unique_ptr<ngl::tbc::PlayerComms<MyCommandPayload>>> players;
+  };
+
+  auto p1 = std::make_unique<MyComms>("Player 1", input_comms);
+  auto p2 = std::make_unique<MyComms>("Player 2", GetComms(2));
+
+  std::vector<std::unique_ptr<MyComms>> players;
   players.emplace_back(std::move(p1));
   players.emplace_back(std::move(p2));
 
-  auto l = ngl::tbc::Layout{{{{0}}, {{1}}}};
+  auto b  = MyBattle{0, default_layout};
+  auto bs = MyScheduler{std::move(players)};
 
-  auto b  = MyBattle{0, l};
-  auto bs = ngl::tbc::BattleScheduler<MyCommands, MyBattle, MyEvents>{std::move(players)};
+  bs.SetCommandValidator(default_validator);
+  bs.SetActionTranslator(default_translator);
 
-  bs.SetCommandValidator([](std::size_t, const std::vector<MyCommandPayload> &payload, const std::vector<MyCommands> &) { return payload; });
-  bs.SetActionTranslator([](const std::vector<MyCommands> &commands) {
-    auto out = std::vector<MyAction>{};
-    for (const auto &c : commands) {
-      int move = std::visit([](auto &&p) {
-        using T = std::decay_t<decltype(p)>;
-        if constexpr (std::is_same_v<T, RockCommand>) {
-          return 0;
-        } else if constexpr (std::is_same_v<T, PaperCommand>) {
-          return 1;
-        } else if constexpr (std::is_same_v<T, ScissorsCommand>) {
-          return 2;
-        } else {
-          throw std::logic_error{"invalid move"};
-        }
-      },
-                            c.payload);
+  bs.SetHandler<ngl::tbc::DefaultEvents::TurnsEnd>(default_turnend);
 
-      out.push_back(GetAction({c.player, 0}, {}, move));
-    }
-    return out;
-  });
-  // const auto commands = bs.RequestCommands({0, 1});
-  // auto actions        = bs.GetActions(commands);
-
-  // for (const auto &r : commands) {
-  //   static_assert(std::is_same_v<std::decay_t<decltype(r.payload)>, MyCommandPayload>);
-  //   std::visit([](auto &&p) {
-  //     using T = std::decay_t<decltype(p)>;
-  //     if constexpr (std::is_same_v<T, RockCommand>) {
-  //       std::cout << "Rock\n";
-  //     } else if constexpr (std::is_same_v<T, PaperCommand>) {
-  //       std::cout << "Paper\n";
-  //     } else if constexpr (std::is_same_v<T, ScissorsCommand>) {
-  //       std::cout << "Scissors\n";
-  //     } else {
-  //       throw std::logic_error{"invalid comand"};
-  //     }
-  //   },
-  //              r.payload);
-  //   std::cout << r.player << "\n";
-  // }
-
-  // ngl::tbc::Turn<MyBattle> turn{actions};
-  bs.SetHandler<ngl::tbc::DefaultEvents::TurnsEnd>([=](ngl::tbc::DefaultEvents::TurnsEnd) { return MyAction{MyDefEffect{{resolve_rps_effect}, {}}}; });
-  // bs.RunTurn(turn, b);
   auto winners = bs.RunBattle(b);
   std::cout << "Winner: Player " << winners.at(0) + 1 << "\n";
 
   return 0;
+}
+
+void test_battle_end() {
+
+  auto p1 = std::make_unique<MyComms>("Player 1", GetComms(1));
+  auto p2 = std::make_unique<MyComms>("Player 2", GetComms(2));
+
+  std::vector<std::unique_ptr<MyComms>> players;
+  players.emplace_back(std::move(p1));
+  players.emplace_back(std::move(p2));
+
+  auto b  = MyBattle{0, default_layout};
+  auto bs = MyScheduler{std::move(players)};
+  bs.SetCommandValidator(default_validator);
+  bs.SetActionTranslator(default_translator);
+  bs.SetHandler<ngl::tbc::DefaultEvents::TurnsEnd>(default_turnend);
+
+  auto winners = bs.RunBattle(b);
+
+  assert(winners.size() == 1);
+  assert(winners.at(0) == 1);
+}
+
+void test_user_event() {
+  auto p1 = std::make_unique<MyComms>("Player 1", GetComms(1));
+  auto p2 = std::make_unique<MyComms>("Player 2", GetComms(2));
+
+  std::vector<std::unique_ptr<MyComms>> players;
+  players.emplace_back(std::move(p1));
+  players.emplace_back(std::move(p2));
+
+  auto b  = MyBattle{0, default_layout};
+  auto bs = MyScheduler{std::move(players)};
+  bs.SetCommandValidator(default_validator);
+  bs.SetActionTranslator(int_event_translator);
+  bs.SetHandler<ngl::tbc::DefaultEvents::TurnsEnd>(default_turnend);
+  bs.SetHandler<int>(default_intevent);
+
+  auto winners = bs.RunBattle(b);
+
+  assert(winners.size() == 1);
+  assert(winners.at(0) == 1);
 }
---
+++ source/util.h
@@ -0,0 +1,18 @@
+#ifndef NGL_UTIL_H
+#define NGL_UTIL_H
+
+namespace ngl {
+
+[[noreturn]] inline void unreachable() {
+  // Uses compiler specific extensions if possible.
+  // Even if no extension is used, undefined behavior is still raised by
+  // an empty function body and the noreturn attribute.
+#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+  __assume(false);
+#else // GCC, Clang
+  __builtin_unreachable();
+#endif
+}
+} // namespace ngl
+
+#endif
