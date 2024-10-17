248e0b7563f83bcb94d0ba7bbff6bcdc20883ccf
ningalu <thomasholster2012@gmail.com>
2024-10-17 19:55:03 +1300

    merge qyueued commands

--- include/tbc/CommandQueue.hpp
+++ include/tbc/CommandQueue.hpp
@@ -15,6 +15,11 @@ struct CommandQueue {
   void BufferCommand(const TCommand &command) {
     static_commands.push_back(command);
   }
+
+  void Merge(const CommandQueue<TCommand> &q) {
+    dynamic_commands.insert(dynamic_commands.begin(), q.dynamic_commands.begin(), q.dynamic_commands.end());
+    static_commands.insert(static_commands.begin(), q.static_commands.begin(), q.static_commands.end());
+  }
 };
 } // namespace ngl::tbc
 
