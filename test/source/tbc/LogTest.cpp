29aeb1232a7cf37f64ec7a07bea822a789622dd3
ningalu <thomasholster2012@gmail.com>
2024-10-26 15:03:54 +1300

    add test

--- test/source/tbc/LogTest.cpp
+++ test/source/tbc/LogTest.cpp
@@ -9,4 +9,6 @@ TEST_CASE("Logs store messages", "[Log]") {
   const auto original_message = std::string{"message"};
   ngl::tbc::Log log{1, original_message};
   const auto retrieved_message = log.Retrieve(0);
+  CHECK(retrieved_message.has_value());
+  CHECK(retrieved_message.value() == original_message);
 }
