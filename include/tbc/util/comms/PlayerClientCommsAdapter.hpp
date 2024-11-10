a7dfdde709b4a8f2877ec0f45b95023cb3d24738
ningalu <thomasholster2012@gmail.com>
2024-11-10 17:10:20 +1300

    Add helper to invert Comms polling dependency

---
+++ include/tbc/util/comms/PlayerClientCommsAdapter.hpp
@@ -0,0 +1,37 @@
+#ifndef TBC_UTIL_COMMS_PLAYERCLIENTCOMMSADAPTER_HPP
+#define TBC_UTIL_COMMS_PLAYERCLIENTCOMMSADAPTER_HPP
+
+namespace ngl::tbc::comms {
+template <typename TBattle>
+class PlayerClientCommsAdapter {
+  using TCommand           = typename TBattle::Command;
+  using TCommandResult     = typename TBattle::CommandResult;
+  using TCommandPayload    = typename TCommand::Payload;
+  using TCommandPayloadSet = typename TCommand::PayloadTypeSet;
+  using TPlayerComms       = typename TBattle::TPlayerComms;
+
+public:
+  PlayerClientCommsAdapter(std::size_t players) : proxies{players} {}
+  [[nodiscard]] std::vector<TPlayerComms> MakePlayerComms() {
+    std::vector<TPlayerComms> out;
+    for (auto &proxy : proxies) {
+      auto player = PlayerComms{
+        "Proxy Player", // TODO: fix this naming sometime
+        [&proxy](const TCommandPayloadSet &valid, const Battle &battle) {
+          return proxy.CommsRequestCallback(valid, battle);
+        }
+      };
+      player.SetResponseHandler(
+        [&proxy](const TCommandResult &result) {
+          proxy.CommsResponseCallback(result);
+        }
+
+      );
+      out.push_back(player);
+    }
+    return out;
+  }
+
+  std::vector<PlayerClientProxy<TBattle>> proxies;
+};
+} // namespace ngl::tbc::comms
---
+++ include/tbc/util/comms/PlayerClientProxy.hpp
@@ -0,0 +1,70 @@
+#ifndef TBC_UTIL_COMMS_PLAYERCLIENTPROXY_HPP
+#define TBC_UTIL_COMMS_PLAYERCLIENTPROXY_HPP
+
+#include <condition_variable>
+#include <mutex>
+#include <vector>
+
+namespace ngl::tbc::comms {
+template <typename TBattle>
+class PlayerClientProxy {
+  using TCommand           = typename TBattle::Command;
+  using TCommandResult     = typename TBattle::CommandResult;
+  using TCommandPayload    = typename TCommand::Payload;
+  using TCommandPayloadSet = typename TCommand::PayloadTypeSet;
+
+public:
+  [[nodiscard]] bool NeedsCommands() {
+    std::unique_lock<std::mutex> lock(request_mutex_);
+    return valid_payloads_.has_value();
+  }
+
+  [[nodiscard]] TCommandResult ProvideCommands(const std::vector<TCommandPayload> &payloads) {
+    {
+      std::unique_lock<std::mutex> request_lock(request_mutex_);
+      request_ = payloads;
+      request_ready_.notify_all();
+    }
+
+    std::unique_lock<std::mutex> response_lock(response_mutex_);
+    response_ready_.wait(response_lock, [this]() {
+      return response_.has_value();
+    });
+    const auto out = response_.value();
+    response_      = std::nullopt;
+    return out;
+  }
+
+  [[nodiscard]] std::vector<TCommandPayload> CommsRequestCallback(const TCommandPayloadSet &valid_payloads) {
+    std::unique_lock<std::mutex> lock(request_mutex_);
+    valid_payloads_ = valid_payloads;
+    request_ready_.wait(lock, [this]() {
+      return request_.has_value();
+    });
+
+    const auto out  = request_.value();
+    request_        = std::nullopt;
+    valid_payloads_ = std::nullopt;
+    return out;
+  }
+
+  void CommsResponseCallback(const CommandResult &result) {
+    std::unique_lock<std::mutex> lock(response_mutex_);
+    response_ = result;
+    response_ready_.notify_all();
+  }
+
+protected:
+  std::mutex request_mutex_;
+  std::condition_variable request_ready_;
+  std::optional<std::vector<TCommandPayload>> request_;
+  std::optional<TCommandPayloadSet> valid_payloads_;
+
+  std::mutex response_mutex_;
+  std::condition_variable response_ready_;
+  std::optional<TCommandResult> response_;
+};
+
+} // namespace ngl::tbc::comms
+
+#endif
